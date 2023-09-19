79430014e2f63f3d676ad11c0554957c181808e7
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-09-19 20:04:30 +0200

    use cart context to display no of items in cart

--- src/components/Layout/HeaderCartButton/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.jsx
@@ -1,14 +1,22 @@
+import { useContext } from 'react';
 import { ReactComponent as CartIcon } from '../../../assets/icons/cart-icon.svg';
+import CartContext from '../../../store/cart-context';
 import './header-cart-button.css';
 
 const HeaderCartButton = ({ showCart, children }) => {
+  const cartContext = useContext(CartContext);
+
+  const numberOfCartItems = cartContext.items.reduce((acc, item) => {
+    return acc + item.amount;
+  }, 0);
+
   return (
     <button className='button' onClick={showCart}>
       <span className='icon'>
         <CartIcon />
       </span>
       <span>{children}</span>
-      <span className='badge'>3</span>
+      <span className='badge'>{numberOfCartItems}</span>
     </button>
   );
 };
