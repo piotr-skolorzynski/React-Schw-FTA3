b27c18a085c70044a1b4848b6f24de974cc0cefa
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-09-22 13:51:13 +0200

    add error handling when fetching meals

--- src/components/Meals/AvailableMeals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals/AvailableMeals.jsx
@@ -7,27 +7,29 @@ import './available-meals.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const fetchMeals = async () => {
-    setIsLoading(true);
-    const response = await fetch(
-      'https://order-food-app-schwarzmuller-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
-    );
-    const loadedMeals = [];
-
-    const data = await response.json();
-
-    for (const key in data) {
-      loadedMeals.push({
-        id: key,
-        name: data[key].name,
-        price: data[key].price,
-        description: data[key].description,
-      });
+    try {
+      const response = await fetch(
+        'https://order-food-app-schwarzmuller-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
+      );
+      const loadedMeals = [];
+      const data = await response.json();
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          price: data[key].price,
+          description: data[key].description,
+        });
+      }
+      setMeals(loadedMeals);
+    } catch (error) {
+      setError('Something went wrong');
+    } finally {
+      setIsLoading(false);
     }
-    setIsLoading(false);
-
-    setMeals(loadedMeals);
   };
 
   useEffect(() => {
@@ -36,26 +38,29 @@ const AvailableMeals = () => {
 
   return (
     <section className='meals'>
-      {meals.length && (
-        <Card>
-          <ul>
-            {isLoading && <LoadingSpinner />}
-            {!isLoading &&
-              meals.length &&
-              meals.map((meal) => {
-                return (
-                  <MealItem
-                    key={meal.id}
-                    id={meal.id}
-                    name={meal.name}
-                    description={meal.description}
-                    price={meal.price}
-                  />
-                );
-              })}
-          </ul>
-        </Card>
-      )}
+      <Card>
+        <ul>
+          {isLoading && <LoadingSpinner />}
+          {!isLoading &&
+            meals.length &&
+            meals.map((meal) => {
+              return (
+                <MealItem
+                  key={meal.id}
+                  id={meal.id}
+                  name={meal.name}
+                  description={meal.description}
+                  price={meal.price}
+                />
+              );
+            })}
+          {error && (
+            <section>
+              <p className='meals-error'>{error}</p>
+            </section>
+          )}
+        </ul>
+      </Card>
     </section>
   );
 };
--- src/components/shared/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/shared/LoadingSpinner/LoadingSpinner.jsx
@@ -3,7 +3,7 @@ import './loading-spinner.css';
 const LoadingSpinner = () => {
   return (
     <div className='lds-roller-container'>
-      <div class='lds-roller'>
+      <div className='lds-roller'>
         <div></div>
         <div></div>
         <div></div>
